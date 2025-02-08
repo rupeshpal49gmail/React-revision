626da7cdc1fc9b11a67ef7456bb2a4914b7c32cb
Rupesh Pal <rupeshpal49@gmail.com>
2025-02-08 23:57:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Hello React</h1>
+        <h2 id="counter">Counter value : 0</h2>
+        <button id="btn1">Add Value</button>
+        <br />
+        <button id="btn2">Remove Value</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
